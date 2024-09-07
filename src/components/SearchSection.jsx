50b7add21d331284cdebb5e1b830d65aa3fe9e7e
wenodh <vinodkumara3@gmail.com>
2024-09-07 15:54:52 +0530

    css fix for search results

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -6,9 +6,9 @@ const SearchSection = () => {
   const { searchedSongs } = useContext(MusicContext);
   return (
     <div
-      className={`fixed left-0 right-0 bottom-0 top-0 flex justify-center items-center flex-wrap gap-4 bg-white bg-opacity-50 backdrop-blur-lg ${
+      className={`fixed left-0 right-0 bottom-0 top-0 py-24 flex justify-center items-center flex-wrap gap-4 bg-white bg-opacity-50 backdrop-blur-lg ${
         searchedSongs.length === 0 ? "-translate-y-[1200px]" : "translate-y-0"
-      } transition-all duration-500 ease-linear z-10`}
+      } transition-all duration-500 ease-linear z-10 overflow-y-auto`}
     >
       {searchedSongs?.map((song) => (
         <SongItem key={song.id} {...song} />
